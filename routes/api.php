52c7bc9ac843e9952d4467c7328eca8964b44b84
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2021-07-09 16:17:15 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login','ApiController@Login');
 Route::post('/signup','ApiController@SignUp');
-Route::post('/categories','ApiController@Categories');
+Route::get('/categories','ApiController@Categories');
 Route::get('/providers','ApiController@Providers');
 Route::get('/provider/{provider}','ApiController@Provider');
 Route::get('/book/{book}','ApiController@BookServices');
