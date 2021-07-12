66bb131454a3dc3304db9b242d4258efee005bb1
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2021-07-12 16:46:38 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -213,14 +213,14 @@
                     breakpoint: 600,
                     settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 3
+                        slidesToScroll: 2
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
                         slidesToShow: 2,
-                        slidesToScroll:3
+                        slidesToScroll: 2
                     }
                 }
             ]
